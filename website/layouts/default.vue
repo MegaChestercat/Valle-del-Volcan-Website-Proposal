ce1bbc75710d8e4525eab238b3ee13afc4e558bc
Mario Arturo Sanchez Ruelas <marioarturosanchez@gmail.com>
2024-04-09 17:51:15 -0600

    Starting to add the default view

---
+++ website/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <v-layout>
+            <v-app-bar>
+                <NuxtLink to="/">
+                    <v-img></v-img>
+                    <v-app-bar-title>Valle del Volcán</v-app-bar-title>
+                </NuxtLink>
+            </v-app-bar>
+            <v-main>
+
+            </v-main>
+        </v-layout>
+    </div>
+</template>
+<script>
+</script>
+<style scoped>
+</style>
