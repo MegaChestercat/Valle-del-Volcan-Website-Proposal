41c3637ea3000795ac0abb64cb6f5fd39172f8e7
Mario Arturo Sanchez Ruelas <marioarturosanchez@gmail.com>
2024-04-09 22:37:22 -0600

    AppBar addition

--- website/layouts/default.vue
+++ website/layouts/default.vue
@@ -3,11 +3,40 @@
         <v-layout>
             <v-app-bar>
                 <NuxtLink to="/">
-                    <v-img></v-img>
-                    <v-app-bar-title>Valle del Volcán</v-app-bar-title>
+                    <v-img src="~~/assets/logo.jpg" class="d-inline-flex"></v-img>
+                    <v-app-bar-title class="ml-5 d-inline-flex">Valle del Volcán</v-app-bar-title>
                 </NuxtLink>
+
+                <v-spacer></v-spacer>
+
+                <v-btn to="/" variant="plain">
+                    <p>Inicio</p>
+                </v-btn>
+                <v-menu>
+                    <template>
+                        
+                    </template>
+                </v-menu>
+                <v-btn to="/" variant="plain">
+                    <p>Acerca De</p>
+                </v-btn>
+                <v-btn to="/" variant="plain">
+                    <p>Tienda</p>
+                </v-btn>
+                <v-btn to="/" variant="plain">
+                    <p>Rutas</p>
+                </v-btn>
+                <v-btn to="/" variant="plain">
+                    <p>Contacto</p>
+                </v-btn>
+                <v-btn to="/" variant="plain" icon>
+                    <v-icon>mdi-account</v-icon>
+                </v-btn>
+                <v-btn to="/" variant="plain" icon>
+                    <v-icon>mdi-cart-outline</v-icon>
+                </v-btn>
             </v-app-bar>
-            <v-main>
+            <v-main class="d-flex justify-center">
 
             </v-main>
         </v-layout>
