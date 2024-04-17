4526f81436981d15ececa290a4acc39bf0c2a6ab
Omar Atriano <omar.atrianova@udlap.com>
2024-04-17 17:36:38 -0600

    Rutas

--- website/pages/rutas.vue
+++ website/pages/rutas.vue
@@ -1,7 +1,9 @@
-<!-- rutas.vue -->
 <template>
-  <div class="component-background" style="display:flex; align-items:center;  justify-content:center;">
-    <v-container fluid > <!-- "fluid" hará que el contenedor se expanda al ancho disponible -->
+  <div class="component-background" style="display:flex; flex-direction:column; align-items:center; justify-content:center;">
+    <!-- Texto "Routes1" ahora está por encima del contenedor principal -->
+    <p class="text-center text-h3 font-weight-bold my-10">Rutas</p>
+    
+    <v-container fluid> <!-- "fluid" hará que el contenedor se expanda al ancho disponible -->
       <v-row justify="center">
         <!-- Columnas para la primera tarjeta, ahora tomando un ancho más grande en pantallas grandes -->
         <v-col cols="12" lg="6" xl="5">
@@ -24,24 +26,39 @@
           </v-card>
         </v-col>
 
-        <!-- Columnas para la imagen del mapita sin tarjeta, ahora tomando un ancho más grande en pantallas grandes -->
+        <!-- Columnas para la imagen del mapita, ahora dentro de una tarjeta -->
         <v-col cols="12" lg="6" xl="5">
-          <v-img
-            class="map-image" 
-            src="~/assets/mapita.png"
-            alt="Mapita"
-          ></v-img>
+          <v-card class="elevation-5">
+            <br>
+            <v-img
+              class="map-image" 
+              src="~/assets/mapita2.png"
+              alt="Mapita"
+              height="300px"
+            ></v-img>
+            <v-card-text>
+              Zonas
+              <br>
+              •San Andrés Cholula
+              <br>
+              •San Pedro Cholula
+              <br>
+              •Plaza San Diego
+              <br>
+              •Zerezotla
+              <br>
+              •Lomas de Angelopolis
+              <br>
+              •Angelopolis
+              <br>
+              •Central de Abastos
+            </v-card-text>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
   </div>
 </template>
-
-<script>
-export default {
-  // Opciones y lógica del componente
-}
-</script>
 
 <style scoped>
 .component-background {
@@ -52,10 +69,8 @@ export default {
 .map-image {
   max-width: 100%;
   height: auto;
-  margin-bottom: 1.5rem;
 }
 
-/* Ajusta el ancho máximo del contenedor para hacerlo más ancho en pantallas grandes */
 .v-container {
   max-width: 1400px; /* Ajusta este valor según tus necesidades */
 }
