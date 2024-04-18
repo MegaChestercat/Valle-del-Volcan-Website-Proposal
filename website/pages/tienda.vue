42e3d60f98b44e1b4674e9f9ae42bda7cbace52b
AndresCabralTB <andres.cabralrs@udlap.mx>
2024-04-18 12:46:35 -0600

    Store

--- website/pages/tienda.vue
+++ website/pages/tienda.vue
@@ -1,3 +1,10 @@
 <template>
-    
+<div class="component-background" style="display:flex; flex-direction:column; align-items:center; justify-content:center;">
+   
+    <p class="text-center text-h3 font-weight-bold my-10">{{ $t("header_3") }}</p>
+</div>
+
+
+
 </template>
+
